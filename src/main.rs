4a499b89fc57262d81bfa359d443c9edb9453da4
Magdy, Ismail (ext) (DI SW ICS MNA RD HAV1 A) <ismail00882@yahoo.com>
2026-09-21 23:35:04 +0300

    change accept, listening to be ran in a loop to continously accept connections

--- src/main.rs
+++ src/main.rs
@@ -6,35 +6,34 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     println!("server is listening on adress {}", address);
 
+    loop {
+        let (mut client_stream, client_adresss) = stream.accept()?;
 
-    let (mut client_stream, client_adresss) = stream.accept()?;
+        println!(
+            "connection accepted with address {} and port {}",
+            client_adresss.ip(),
+            client_adresss.port()
+        );
 
-    println!(
-        "connection accepted with address {} and port {}",
-        client_adresss.ip(),
-        client_adresss.port()
-    );
+        let body = "<!DOCTYPE html><html><body>Hello World from RUST!</body></html>";
 
-    let body = "<!DOCTYPE html><html><body>Hello World from RUST!</body></html>";
-
-    let response_header = format!(
-        "HTTP/1.1 200 OK
+        let response_header = format!(
+            "HTTP/1.1 200 OK
 Date: Mon, 21 Sep 2026 23:26:00 GMT
 Server: Apache/2.4.41 (Ubuntu)
 Content-Type: text/html; charset=UTF-8
 Content-Length: {}
 Connection: keep-alive",
-        body.len()
-    );
+            body.len()
+        );
 
-    let response = (response_header + "\r\n\r\n") + body;
+        let response = (response_header + "\r\n\r\n") + body;
 
-    println!("response is {}", response);
+        let written_bytes = client_stream.write(response.as_bytes())?;
+        println!("response of size {}, is written back to the client", {
+            written_bytes
+        });
 
-    let written_bytes = client_stream.write(response.as_bytes())?;
-    println!("response of size {}, is written back to the client", {
-        written_bytes
-    });
-
-    Ok(())
+        println!("==============================");
+    }
 }
